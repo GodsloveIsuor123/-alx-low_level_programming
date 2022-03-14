601e3a4f236a1a256882d398a64a787717c4cc08
root <godsloveisuor@gmail.com>
2022-03-14 10:47:11 -0700

    hmm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sm;
+
+	for (sm = 'a'; sm <= 'z'; ch++)
+		putchar(sm);
+	putchar("\n");
+	return (0);	
+}
