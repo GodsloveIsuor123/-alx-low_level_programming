81e07fc3c4b8ea5b7d4e179ab25f5c143d14cb78
root <godsloveisuor@gmail.com>
2022-03-24 08:29:46 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,22 @@
-nes (20 sloc)  304 Bytes
-   
-#include "main.h"
-/**
- * string_toupper - Changes all lowercase letters to uppercase
- * @p: The string will be modified
- * Return: char var
- */
-char *string_toupper(char *p)
+#include <stdio.h>
+int main(void)
 {
-	int a = 0;
+	int n;
+	int a[5];
+	int *p;
 
-	while (p[a])
-	{
-		if (p[a] >= 97 && p[a] <= 122)
-		{
-			p[a] -= 32;
-		}
-		a++;
-	}
-	return (p);
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 *   - you are not allowed to modify p
+	 *   - only one statement
+	 *   - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
