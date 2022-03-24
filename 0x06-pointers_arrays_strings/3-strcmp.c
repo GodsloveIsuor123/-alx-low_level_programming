3db1d24b09b2e2b47e86a7e29a2ba56d5612f15f
root <godsloveisuor@gmail.com>
2022-03-24 06:39:43 -0700

    @s1s2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,6 @@
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0, b = 0, c = 0, r = 0, lim;
-
 	while (s1[a])
 	{
 		a++;
@@ -31,16 +30,13 @@ int _strcmp(char *s1, char *s2)
 		{
 			c++;
 			continue;
-			{
-				else
-				{
-					r = s1[c] - s2[c];
-					break;
-				}
-				c++;
-			}
-			return (r);
 		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+		c++;
 	}
+	return (r);
 }
-
